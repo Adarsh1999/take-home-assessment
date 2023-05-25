096c9e9f7a743e8dafb8fe10bf7ea460cce38e9f
Adarsh <adarshguptamaurya@gmail.com>
2023-05-25 22:31:40 +0530

    feat: lint rule update & prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
     'plugin:tailwindcss/recommended',
     'plugin:vitest-globals/recommended',
     'airbnb',
+    'prettier/@typescript-eslint',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -38,43 +39,55 @@ module.exports = {
     semi: ['error', 'never'],
 
     'react/react-in-jsx-scope': 'off',
-    'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+    'react/jsx-filename-extension': [
+      'error',
+      { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+    ],
     'react/jsx-one-expression-per-line': 'off',
-    'react/jsx-curly-spacing': ['error', {
-      when: 'always',
-      attributes: {
-        when: 'never',
-      },
-      children: {
+    'react/jsx-curly-spacing': [
+      'error',
+      {
         when: 'always',
+        attributes: {
+          when: 'never',
+        },
+        children: {
+          when: 'always',
+        },
       },
-    }],
+    ],
 
     'import/extensions': ['off', 'never'],
-    'import/order': ['error', {
-      groups: [
-        'builtin',
-        'external',
-        'internal',
-        ['sibling', 'parent'],
-        'index',
-        'object',
-        'type',
-        'unknown',
-      ],
-      'newlines-between': 'always',
-    }],
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          ['sibling', 'parent'],
+          'index',
+          'object',
+          'type',
+          'unknown',
+        ],
+        'newlines-between': 'always',
+      },
+    ],
     'import/first': 'warn',
     'import/exports-last': 'warn',
     'import/prefer-default-export': 'warn',
     'no-unused-vars': 'off',
     'unused-imports/no-unused-imports': 'error',
-    'unused-imports/no-unused-vars': ['warn', {
-      vars: 'all',
-      varsIgnorePattern: '^_',
-      args: 'after-used',
-      argsIgnorePattern: '^_',
-    }],
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
 
     'no-shadow': 'off',
     '@typescript-eslint/no-shadow': ['error'],
@@ -84,7 +97,12 @@ module.exports = {
   },
   overrides: [
     {
-      files: ['*.config.ts', '*.config.js', 'src/tests/**/*.ts', 'src/tests/**/*.tsx'],
+      files: [
+        '*.config.ts',
+        '*.config.js',
+        'src/tests/**/*.ts',
+        'src/tests/**/*.tsx',
+      ],
       rules: {
         'import/no-extraneous-dependencies': 'off',
       },
